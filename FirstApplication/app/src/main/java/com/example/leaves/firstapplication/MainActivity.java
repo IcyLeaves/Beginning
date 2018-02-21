7736b7a3ca2ceadb7d78b52651be08881e240535
IcyLeaves <leavescy@126.com>
2018-02-21 13:23:14 +0800

    TextView-跑马灯效果简单实现

--- FirstApplication/app/src/main/java/com/example/leaves/firstapplication/MainActivity.java
+++ FirstApplication/app/src/main/java/com/example/leaves/firstapplication/MainActivity.java
@@ -6,16 +6,10 @@ import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView txtLink;
-    private TextView txtCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtLink=findViewById(R.id.txtR6);
-        txtLink.setMovementMethod(LinkMovementMethod.getInstance());
-        txtCall=findViewById(R.id.txtPhone);
-        txtCall.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
